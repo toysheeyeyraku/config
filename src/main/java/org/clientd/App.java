d26fc6180dd8088709dfd92faf0274c8d0576ad3
toysheeyeyraku <kirpich1337228@gmail.com>
2019-12-07 19:46:22 +0200

    xe

--- src/main/java/org/clientd/App.java
+++ src/main/java/org/clientd/App.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
